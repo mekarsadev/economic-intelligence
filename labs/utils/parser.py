4a0da3b56f7801a3560cb1b3de26d1ea75501640
Sugeng D. Cahyo <34049790+sugengdcahyo@users.noreply.github.com>
2023-05-19 01:02:40 +0700

    add method preprocesing

--- labs/utils/parser.py
+++ labs/utils/parser.py
@@ -10,3 +10,12 @@ def datestring2unix(datestring: str) -> int:
     unix_timestamp = int(time.mktime(date_object.timetuple()))
 
     return unix_timestamp
+
+
+class dict2obj(object):
+    def __init__(self, d):
+        for a, b in d.items():
+            if isinstance(b, (list, tuple)):
+                setattr(self, a, [dict2obj(x) if isinstance(x, dict) else x for x in b])
+            else:
+                setattr(self, a, dict2obj(b) if isinstance(b, dict) else b)
