612f5160d9ca2701e8119eea9d0aff12b6c8ec61
Sugeng D. Cahyo <34049790+sugengdcahyo@users.noreply.github.com>
2023-06-07 03:32:23 +0700

    Initial ofx modules

---
+++ ofx_modules/api.py
@@ -0,0 +1,37 @@
+import requests
+
+
+class OFX:
+    def __init__(self, scc, bcc, start_date, end_date):
+        self.scc = scc
+        self.bcc = bcc
+        self.start_date = start_date
+        self.end_date = end_date
+        self.querystring = {
+            "DecimalPlace": 6,
+            "ReportingInterval": "Daily",
+            "format": "json",
+        }
+
+    def fetch(self):
+        url = f"https://api.ofx.com/PublicSite.ApiService//SpotRateHistory/{self.scc}/{self.bcc}/{self.start_date}/{self.end_date}"
+
+        headers = {
+            "cookie": "AWSALB=im7M9BzJ03Mq7i1ap4yAh2GuXbGpTfZkF5%2BbE2Y6C1yuCyCXjQPkpjntorRhqtIib7SPD2j5%2FHyxjQtutA%2BxoqOaZQg7iFXQU92Iel3Up2ZklGGPOmHj3Vuy%2F9ub; AWSALBCORS=im7M9BzJ03Mq7i1ap4yAh2GuXbGpTfZkF5%2BbE2Y6C1yuCyCXjQPkpjntorRhqtIib7SPD2j5%2FHyxjQtutA%2BxoqOaZQg7iFXQU92Iel3Up2ZklGGPOmHj3Vuy%2F9ub",
+            "authority": "api.ofx.com",
+            "accept": "*/*",
+            "accept-language": "en-US,en;q=0.9,id;q=0.8",
+            "dnt": "1",
+            "origin": "https://www.ofx.com",
+            "referer": "https://www.ofx.com/",
+            "sec-fetch-dest": "empty",
+            "sec-fetch-mode": "cors",
+            "sec-fetch-site": "same-site",
+            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+        }
+
+        response = requests.request(
+            "GET", url, data="", headers=headers, params=self.querystring
+        )
+        print(response.status_code)
+        self.data = response
--- src/urls.py
+++ src/urls.py
@@ -2,6 +2,7 @@ from core.api import BaseApi
 
 from .analyzer.viewsets.predict import PredictViewset
 from .index.viewsets.index import IndexViewset
+from .ofx.viewsets.ofx import OFXViewset
 from .yahoo.viewsets.yahoo import YahooViewset
 
 
@@ -11,4 +12,5 @@ def init(app) -> None:
 
     api = BaseApi(app, prefix="/v1/finet")
     api.add_resource(YahooViewset, "/yahoo/charts")
+    api.add_resource(OFXViewset, "/ofx/charts")
     api.add_resource(PredictViewset, "/analyzer/predict")
