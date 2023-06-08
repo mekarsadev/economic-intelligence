08591bd3572e251688690a0710cf66d835f6f63a
Sugeng D. Cahyo <34049790+sugengdcahyo@users.noreply.github.com>
2023-06-08 18:29:58 +0700

    Initial predict process

--- src/analyzer/viewsets/predict.py
+++ src/analyzer/viewsets/predict.py
@@ -1,7 +1,12 @@
 import random
 import time
+from datetime import datetime, timedelta
 
+import torch
 from flask_restful import Resource, request
+
+from labs.utils.data_generator import ofx_dataset
+from src.analyzer.lstm import LSTMModel
 
 
 class PredictViewset(Resource):
@@ -13,12 +18,26 @@ class PredictViewset(Resource):
     ):
         data = request.get_json()
 
+        lstm = LSTMModel(input_size=1, hidden_sizes=[64], output_size=1)
+        lstm.load_state_dict(torch.load("labs/models/USD_10_64.pth"))
+
         if data["model"] not in self.TICKERS:
             return {"message": "ticker not available."}, 400
 
         predicted_value = []
         predicted_timestamp = []
         timestamp = int(time.time())
+
+        end_date = datetime.now().date()
+        start_date = end_date - timedelta(days=10)
+
+        end_date = end_date.strftime("%Y-%m-%d")
+        start_date = start_date.strftime("%Y-%m-%d")
+        # return {'end': None}
+        ofx_sample = ofx_dataset(start_date, end_date)
+        print(ofx_sample)
+        return ofx_sample[["values"]].to_dict()
+
         for _ in range(10):
             random_range = random.uniform(100.5, 240.5)
             timestamp += 86400
